3343289e4a7856dc228b162646a9ce8462508b9a
Glennmark2002 <Glennmark.culibra@gmail.com>
2024-12-10 11:00:26 +0800

    Get all books and books by id

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-import express from 'express'; 
+import express, { response } from 'express'; 
 import mongoose from 'mongoose';  
 
 import { Book } from './models/book.model.js';
@@ -36,6 +36,42 @@ app.post('/books', async (req, res) => {
   }
 
 });   
+
+app.get('/books', async (req, res) => {
+
+  try {
+
+    const books = await Book.find({});
+    
+    res.status(200).json({
+      count: books.length,
+      data: books
+    }); 
+    
+  } catch (error) {
+    console.log(error.message) ;  
+    res.status(500).send({message: error.message});
+
+  }
+
+});  
+
+// get the by the ID
+app.get('/books/:id', async (req, res) => {
+
+  try {
+    
+    const { id } = req.params;   
+    const book = await Book.findById(id);
+    
+    return res.status(200).json(book); 
+    
+  } catch (error) {
+    console.log(error.message) ;  
+    res.status(500).send({message: error.message});
+  }
+
+});  
  
 
 mongoose.connect(mongoURL)
